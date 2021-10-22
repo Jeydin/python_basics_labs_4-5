4fd033cce3637fa8c4e53f562dccbac915d68a45
Jeydin Pham <89167700+Jeydin@users.noreply.github.com>
2021-10-22 20:31:03 +0000

    Fixed it

--- math.py
+++ math.py
@@ -8,4 +8,7 @@ xmod2 = int(x) % 2
 ymod2 = int(y) % 2
 zmod2 = float(z) % 2
 
-print("Product of x and y is " + str(productxy) + "\nProduct of x, y, and z is " + str(productxyz) + "\n" + str(x) + " mod 2 is " + str(xmod2) + "\n" + str(y) + " mod 2 is " + str(ymod2) + "\n" + str(z) + " mod 2 is " + str(zmod2))
+print("Product of x and y is " + str(productxy) +
+      "\nProduct of x, y, and z is " + str(productxyz) + "\n" + str(x) +
+      " mod 2 is " + str(xmod2) + "\n" + str(y) + " mod 2 is " + str(ymod2) +
+      "\n" + str(z) + " mod 2 is " + str(zmod2))
--- variables.py
+++ variables.py
@@ -3,4 +3,5 @@ age = input("How old are you? ")
 lastbirthday = input("What year was your last birthday? ")
 
 birthyear = int(lastbirthday) - int(age)
-print("Your name is " + name + " and you are " + age + " years old\nYou were born in " + str(birthyear))
+print("Your name is " + name + " and you are " + age +
+      " years old\nYou were born in " + str(birthyear))
